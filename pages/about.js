125282e3ecf403d62ac4e2ed007fc3b74927b71e
Chaitanya Pratap Singh <chaitanyapratapsingh24@gmail.com>
2023-12-30 18:12:13 +0530

    contact and about page

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,48 @@
-import React from 'react'
+// pages/about.js
 
-const about = () => {
+import React from 'react';
+import styles from '@/styles/Home.module.css'; // Import your CSS module
+
+const About = () => {
   return (
-    <div>about</div>
-  )
-}
+    <div>
+      <section className={styles.aboutHeader}>
+        <h1 className={styles.title}>Welcome to Hunting Coder!</h1>
+        <p className={styles.para}>
+          Welcome to Hunting Coder, the ultimate hub for exploring the realms of programming and technology!
+          Our platform merges the eloquence of language with the precision of code, offering a vast spectrum
+          of insights, guides, and discoveries.
+        </p>
+        <p className={styles.para}>
+          Whether you're a seasoned developer or just stepping into the world of coding, our blog is designed
+          to be your guiding star through the ever-evolving landscape of tech.
+        </p>
+      </section>
 
-export default about
+      <section className={styles.aboutMain}>
+        <h2 className={styles.title}>Embark on a Journey</h2>
+        <p className={styles.para}>
+          At Hunting Coder, we aim to ignite your curiosity and passion for technology. Join us as we unravel
+          the mysteries of algorithms, celebrate the elegance of code, and explore the innovations shaping
+          our digital future.
+        </p>
+
+        <h2 className={styles.title}>Our Mission</h2>
+        <p className={styles.para}>
+          Our mission is to democratize technology education and make it accessible to everyone. We believe
+          that knowledge should be shared freely, and through our articles, tutorials, and resources, we strive
+          to empower individuals from diverse backgrounds to thrive in the world of programming.
+        </p>
+
+        <h2 className={styles.title}>Connect with Us</h2>
+        <p className={styles.para}>
+          We love to connect with our community! Follow us on social media for updates, tech news, and more.
+          For any inquiries, collaborations, or suggestions, feel free to reach out to us at
+          contact@huntingcoder.com.
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default About;
--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,43 @@
-import React from 'react'
+// pages/contact.js
 
-const contact = () => {
+import React from 'react';
+import styles from '@/styles/Home.module.css'; 
+const Contact = () => {
   return (
-    <div>contact</div>
-  )
-}
+    <div className={styles.contactContainer}>
+      <h1 className={styles.title}>Contact Us</h1>
+      <p className={styles.para}>
+        Have questions, suggestions, or just want to say hello? We'd love to hear from you!
+        Feel free to reach out using the contact details below.
+      </p>
 
-export default contact
+      <div className={styles.contactInfo}>
+        <h2 className={styles.title}>Contact Information</h2>
+        <p className={styles.para}>
+          <strong>Email:</strong> contact@huntingcoder.com
+        </p>
+        <p className={styles.para}>
+          <strong>Phone:</strong> +1 234 567 8901
+        </p>
+        <p className={styles.para}>
+          <strong>Address:</strong> 123 Coding Street, Techville
+        </p>
+        
+      </div>
+
+      {/* Embedded Map */}
+      <div className={styles.mapContainer}>
+        <iframe
+          title="Map"
+          width="400"
+          height="400"
+          loading="lazy"
+          allowFullScreen
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30767217.76788454!2d60.944588889743954!3d19.722674057189707!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1703940039344!5m2!1sen!2sin"
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
